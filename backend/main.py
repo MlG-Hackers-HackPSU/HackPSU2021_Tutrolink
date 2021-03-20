4c6080026b05210f652fbce12ce13303ed2bc344
Hargrav3s <SketchMyHearts@gmail.com>
2021-03-20 03:40:12 -0400

    Added queue functionality, interfacing with MongoDB

--- backend/main.py
+++ backend/main.py
@@ -15,34 +15,74 @@ client = MongoClient(
     password=os.environ["MONGO_PASSWORD"])
 
 
-#Here are the objects that will be sent back and forth
+# Here are the objects that will be sent back and forth
 class Session(BaseModel):
     ID: int
     Topics: List[str]
     Tutors: List[int]
     Queue: List[int]
 
-counters = client.demo.counters
-
 sessions = client.tutrolink.sessions
 
+# lol forever counter
 @app.get("/")
 async def index():
-    counter = counters.find_one()
-    if counter is None:
-        counters.insert_one({'num_visited': 1})
-    counter = counters.find_one_and_update({}, {'$inc': {'num_visited': 1}})
-    return f"This page has been visited {counter['num_visited']} times."
+    return "No, don't search me!"
 
 
 def newID():
     return random.randint(10000,12000)
 
+
 @app.post("/rooms")
 async def createSession():
     id = newID()
     newSession = Session(ID = id,Topics = [],Tutors = [],Queue = [])
-    sessions.insert_one(json(newSession))
-    return newSession.ID
+    sessions.insert_one(dict(newSession))
+    return id
+
+    
+@app.get("/student/{session_id}/join")
+def studentWantsToJoin(session_id):
+    if not sessionExists(session_id):
+        return "Error: invalid session_id"
+    topics = getTopics(session_id)
+
+    # TODO redirect them to fourm with the topics
+
+    return str(topics)
+
+@app.post("/student/{session_id}/join")
+def addStudent(student):
+    # TODO - Add student to DB.
+    return "No Student :("
 
 
+# Takes in a student in JSON format, and inserts into 
+# the beginning of the session queue denoted by session_id.
+def enqueueStudent(session_id, student):
+    sessions.find_one_and_update({"ID" : int(session_id)}, {"$push": {"Queue": int(student)}})
+
+# Dequeues a student from the session_id queue.
+def dequeueStudent(session_id):
+    my_session = Session.parse_obj(sessions.find_one({"ID":int(session_id)}))
+
+    queue = my_session.Queue
+
+    if len(queue) == 0:
+        return None
+    else:
+        popped = queue.pop(0)
+
+    sessions.find_one_and_update({"ID": int(session_id)}, {"$pop": {"Queue": -1}})
+    return popped
+
+def getTopics(session_id):
+    my_session = Session.parse_obj(sessions.find_one({"ID": session_id}))
+    topics = my_session.Topics
+    return list(topics)
+
+def sessionExists(session_id):
+    if sessions.find_one( { "ID": int(session_id) } ):
+        return True
+    return False
