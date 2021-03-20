488f76f4b9620da2e04fb27ca5e0398a031f5309
parkergriep <griep.p@husky.neu.edu>
2021-03-19 21:39:00 -0400

    write small stateful example of using mongo on the backend

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,21 @@ from fastapi import FastAPI
 from datetime import datetime
 import requests
 import json
+import os
+from pymongo import MongoClient
 
 app = FastAPI()
+client = MongoClient(
+    host="database", 
+    username=os.environ["MONGO_USERNAME"], 
+    password=os.environ["MONGO_PASSWORD"])
+
+counters = client.demo.counters
 
 @app.get("/")
 async def index():
-    return "Goodbye world"
+    counter = counters.find_one()
+    if counter is None:
+        counters.insert_one({'num_visited': 1})
+    counter = counters.find_one_and_update({}, {'$inc': {'num_visited': 1}})
+    return f"This page has been visited {counter['num_visited']} times."
