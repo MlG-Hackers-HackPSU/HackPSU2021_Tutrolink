650a01d8b13b0d8c0efa823cc00f2ea56ed891d2
Hargrav3s <Harqrav3s@gmail.com>
2021-03-21 15:55:15 -0400

    Fixed conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,13 +14,8 @@ function App() {
     <CookiesProvider>
       <Router>
         <Switch>
-<<<<<<< HEAD
         <Route path="/dashboard/:session">
             <Dashboard />
-=======
-        <Route path="/admin">
-            <Admin />
->>>>>>> d9be2ef5b9c47c2af2225a1ef3a1e25da2da9ec0
           </Route>
           <Route path="/">
             <Home />
